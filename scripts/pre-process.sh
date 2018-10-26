5a52712397f0d920c2cda0fe596a54d1c95d1461
Simon Elvery <simon@elvery.net>
2018-10-26 20:09:01 +1000

    Just use narrow data
    It's simpler to work with. But split out content for ease of use in R

--- scripts/pre-process.sh
+++ scripts/pre-process.sh
@@ -5,12 +5,7 @@ echo "Combine data files"
 cat dump-mobile.json | jq -c '{id, request, response, device: "mobile"}' > request-response-combined.json
 cat dump-laptop.json | jq -c '{id, request, response, device: "laptop"}' >> request-response-combined.json
 
-echo "Removing payload"
-cat request-response-combined.json | jq -c "{id, device, request: .request|del(.content), response: .response|del(.content) }" | jq -c '[leaf_paths as $path | {"key": $path | join(".") | ascii_downcase, "value": getpath($path)}] | from_entries' > requests-response-combined-no-content.json
-
 echo "Making CSVs"
-# Wide
-cat request-response-combined.json | jq -c "{id, device, request, response}" | jq -c '[leaf_paths as $path | {"key": $path | join(".") | ascii_downcase, "value": getpath($path)}] | from_entries' | jq -sr '(map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @csv' > request-response-combined.csv
-
 # Narrow
-cat request-response-combined.json |  jq --compact-output --raw-output '{id, device, request, response} | [leaf_paths as $path | {id: .id, field: $path | join(".") | ascii_downcase, value: getpath($path)}] | del(.[0]) | .[] | [.id, .field, .value] | @csv' > all_narrow.csv
+cat request-response-combined.json |  jq --compact-output --raw-output '{id, device, request: .request|del(.content), response:.response|del(.content)} | [leaf_paths as $path | {id: .id, field: $path | join(".") | ascii_downcase, value: getpath($path)}] | del(.[0]) | .[] | [.id, .field, .value] | @csv' > meta_narrow.csv
+cat request-response-combined.json |  jq --compact-output --raw-output '{id, request: {content: .request.content}, response:{content:.response.content}} | [leaf_paths as $path | {id: .id, field: $path | join(".") | ascii_downcase, value: getpath($path)}] | del(.[0]) | .[] | [.id, .field, .value] | @csv' > content_narrow.csv
