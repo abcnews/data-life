8783c02295cb7d3ce9f9e77a436d2c49f907e6de
Simon Elvery <simon@elvery.net>
2018-11-01 10:51:57 +1000

    Use rsync — it's way faster

--- scripts/download.sh
+++ scripts/download.sh
@@ -8,29 +8,20 @@ realpath() {
 # Source and set some env variables
 PROJECT_DIR=$(realpath "${BASH_SOURCE%/*}/..")
 DATE=`date +%Y-%m-%d-%H-%M-%S`
-DATA_DIR="$PROJECT_DIR/analysis/proxydata/increment"
+DATA_DIR="$PROJECT_DIR/analysis/data/proxy"
 source "$PROJECT_DIR/server/.envrc"
 
 mkdir -p $DATA_DIR
 
-# Connect and download the mobile data
-echo "Downloading mobile data from $VPN_IP"
-ssh root@$VPN_IP 'docker run --rm --volumes-from mitmproxy -v $(pwd)/backup:/backup ubuntu tar czvf /backup/flows.tar.gz /proxydata/dump.json'
-ssh root@$VPN_IP 'docker logs -t mitmproxy > $(pwd)/backup/proxy-mobile.log'
-rsync -rvP root@$VPN_IP:~/backup/* /tmp
-
-echo "Unpacking mobile data"
-tar -xzf /tmp/flows.tar.gz --directory /tmp
-mv /tmp/proxydata/dump.json "$DATA_DIR/dump-mobile.json"
-mv /tmp/proxy-mobile.log "$DATA_DIR/proxy-mobile.log"
-
-echo 'Tidying up'
-rm -rf /tmp/proxydata
-# rm /tmp/proxy-mobile.log
-
 # Connect and download latest laptop data
 echo "Downloading laptop data"
-docker run --rm --volumes-from mitmproxy -v $DATA_DIR:/backup ubuntu cp /proxydata/dump.json /backup/dump-laptop.json
+docker run --rm --volumes-from mitmproxy -v $DATA_DIR:/backup eeacms/rsync rsync -rvP --no-R /proxydata/dump.json /backup/dump-laptop.json
 docker logs -t mitmproxy > $DATA_DIR/proxy-laptop.log
+
+# Connect and download the mobile data
+echo "Downloading mobile data from $VPN_IP"
+ssh root@$VPN_IP 'docker run --rm --volumes-from mitmproxy -v $(pwd)/backup:/backup ubuntu cp /proxydata/dump.json /backup/dump-mobile.json'
+ssh root@$VPN_IP 'docker logs -t mitmproxy > $(pwd)/backup/proxy-mobile.log'
+rsync -rvPz root@$VPN_IP:~/backup/* $DATA_DIR
 
 echo "Done"
